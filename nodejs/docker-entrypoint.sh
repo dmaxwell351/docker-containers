00c30035b03572194de64a194c4f7ad8bd5ac205
Daniel Maxwell <dmaxwell351@sent.com>
2018-11-17 10:02:24 -0500

    Update docker-entrypoint.sh

--- nodejs/docker-entrypoint.sh
+++ nodejs/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
-mkdir -p /root/.ssh
-cp /config/authorized_keys /root/.ssh/authorized_keys 2>/dev/null || :
-chmod 700 /root/.ssh
-chmod 400 /root/.ssh/authorized_keys 2>/dev/null || :
-chown root. /root/.ssh/authorized_keys 2>/dev/null || :
+mkdir -p /home/ubuntu/.ssh
+cp /config/authorized_keys /home/ubuntu/.ssh/authorized_keys 2>/dev/null || :
+chown -R ubuntu /home/ubuntu/.ssh /home/ubuntu/.ssh/authorized_keys 2>/dev/null || :
+chmod 700 /home/ubuntu/.ssh 2>/dev/null || :
+chmod 600 /home/ubuntu/.ssh/authorized_keys 2>/dev/null || :
 /usr/sbin/sshd -D
