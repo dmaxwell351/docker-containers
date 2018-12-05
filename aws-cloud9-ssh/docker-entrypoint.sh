10e7cebdd9a5f99836bbc3e440905cde57c738cf
Daniel Maxwell <dmaxwell351@sent.com>
2018-12-05 14:36:27 -0500

    Update docker-entrypoint.sh

--- aws-cloud9-ssh/docker-entrypoint.sh
+++ aws-cloud9-ssh/docker-entrypoint.sh
@@ -7,7 +7,7 @@ cp /config/authorized_keys ~/.ssh/authorized_keys 2>/dev/null || :
 chown -R ubuntu ~/.ssh ~/.ssh/authorized_keys 2>/dev/null || :
 chmod 700 ~/.ssh 2>/dev/null || :
 chmod 600 ~/.ssh/authorized_keys 2>/dev/null || :
-exit
+su root
 
 #/usr/sbin/sshd -D -E /config/auth.log
 tail -f /dev/null
