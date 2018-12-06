f1e0ae40bf9e3d1bd4ef4ce154c993732af7f8b2
Daniel Maxwell <dmaxwell351@sent.com>
2018-12-06 07:59:40 -0500

    Update docker-entrypoint.sh

--- aws-cloud9-ssh/docker-entrypoint.sh
+++ aws-cloud9-ssh/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-mkdir -p /home/ubuntu/.ssh
+usermod -u 99 ubuntu 2>/dev/null || :
+curl -o /home/ubuntu/install.sh -L https://raw.githubusercontent.com/c9/install/master/install.sh 2>/dev/null || :
+su ubuntu -c 'bash ~/install.sh' 2>/dev/null || :
+
+mkdir -p /home/ubuntu/.ssh 2>/dev/null || :
 cp /config/authorized_keys /home/ubuntu/.ssh/authorized_keys 2>/dev/null || :
 chown -R ubuntu:ubuntu /home/ubuntu/.ssh /home/ubuntu/.ssh/authorized_keys 2>/dev/null || :
 chmod 700 /home/ubuntu/.ssh 2>/dev/null || :
